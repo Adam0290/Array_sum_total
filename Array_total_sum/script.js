38f85ce5d43b3c10ecf044e388cba2603b8b8fcd
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-04-21 16:51:46 +0100

    Add files via upload

---
+++ Array_total_sum/script.js
@@ -0,0 +1,11 @@
+// Calculate the total sum of a Array >>>>> Example of Javascript Loop //
+
+let arr = [8, 9, 7, 4, 2, 3, 3, 9,]; // Example of a price of list of bought items >>>>> Placed in a Array //
+let sum = 0; // Sum Variable // 
+for (let i = 0; i < arr.length; i++ ) {
+sum += arr[i];
+}
+
+console.log(sum);
+
+// Calculate the bill total // 
